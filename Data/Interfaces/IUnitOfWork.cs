f1b50ba79ec016e1453a85beb256d9c63361029d
MrSampy <mynameisserzheo@gmail.com>
2022-11-03 22:20:01 +0200

    Add interfaces IUnitOfWork, IReceiptDetailRepository,iReceiptRepository,IRepository

---
+++ Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace Data.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        ICustomerRepository CustomerRepository { get; }
+
+        IPersonRepository PersonRepository { get; }
+
+        IProductRepository ProductRepository { get; }
+
+        IProductCategoryRepository ProductCategoryRepository { get; }
+
+        IReceiptRepository ReceiptRepository { get; }
+
+        IReceiptDetailRepository ReceiptDetailRepository { get; }
+        public Task SaveAsync();
+    }
+}
