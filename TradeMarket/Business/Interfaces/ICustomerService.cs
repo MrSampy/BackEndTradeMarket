d62be099a0527c346fc017b3a0fb182cbf9b766f
MrSampy <mynameisserzheo@gmail.com>
2022-11-22 10:08:25 +0200

    Add ICrud and ICustomerService Interfaces

---
+++ TradeMarket/Business/Interfaces/ICustomerService.cs
@@ -0,0 +1,12 @@
+﻿using Business.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface ICustomerService : ICrud<CustomerModel>
+    {
+        Task<IEnumerable<CustomerModel>> GetCustomersByProductIdAsync(int productId);
+        Task<IEnumerable<CustomerModel>> GetByProductIdAsync(int productId);
+    }
+}
