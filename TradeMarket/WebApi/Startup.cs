12882fd0381df26e8d35a2341355a5cc61c32e5d
MrSampy <mynameisserzheo@gmail.com>
2022-12-04 16:23:26 +0200

    Add Startup.cs

--- TradeMarket/WebApi/Startup.cs
+++ TradeMarket/WebApi/Startup.cs
@@ -1,21 +1,50 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AutoMapper;
+using Business;
+using Business.Interfaces;
+using Business.Services;
+using Data.Data;
+using Data.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 
 namespace WebApi
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration.GetConnectionString("Market");
+
+            services.AddControllers();
+
+            services.AddDbContext<TradeMarketDbContext>(t => t.UseSqlServer(connection));
+
+            services.AddAutoMapper(typeof(AutomapperProfile).Assembly);
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddTransient<ICustomerService, CustomerService>();
+
+            services.AddTransient<IProductService, ProductService>();
+
+            services.AddTransient<IReceiptService, ReceiptService>();
+
+            services.AddTransient<IStatisticService, StatisticService>();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,12 +55,17 @@ namespace WebApi
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
-            });
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI();//path:/swagger/
         }
     }
 }
